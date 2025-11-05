93fe2a564b75c2c013cb569c15b28ee3a8d7b427
jdszzz <cvalejandrobg@gmail.com>
2025-11-05 15:52:22 +0100

    Seguir con la lógica

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: albelmon <albelmon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:35:36 by albelmon          #+#    #+#             */
-/*   Updated: 2025/11/04 14:48:20 by albelmon         ###   ########.fr       */
+/*   Updated: 2025/11/05 15:49:57 by albelmon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,53 @@
 //En line quiero guardar el resto del archivo. Lo mueestro con buffer y almaceno el resto en line.
 char	*get_next_line(int fd)
 {
+	static char	*rest;
 	char		*line;
 	char		*buffer;
-	static int	i;
+	size_t		i;
+	size_t		n_bytes;
 	
+	//Comprobación de argumentos
 	if (fd <= 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	i = -1;
+	i = 0;
+	//Reserva de espacio con malloc para el buffer. Del tamaño que nos indiquen
 	buffer = malloc(BUFFER_SIZE);
 	if (!buffer)
 		return (NULL);
-	read(fd, buffer, BUFFER_SIZE);
-	while (buffer[++i] != '\n')
-		line[i] = buffer[i];
-	line[i] = '\n';
-	line[++i] = '\0';
+	//Inicialización de la variable estatica del contenido que ya hemos leido. En la primera lectura deberá de ser null
+	if (rest == NULL)
+		rest = "";
+	//Lógica
+	n_bytes = read(fd, buffer, BUFFER_SIZE);
+	while (ft_line_found(buffer, n_bytes) == -1)
+		ft_strlcpy(line, buffer, n_bytes + 1);
+	line[n_bytes + 1] = '\0';
 	free(buffer);
 	return (line);
 }
 
+
+//Funcion que mira si hay un salto de linea dentro del buffersize indicado
+//Retorna -1 si NO ha encontrado un salto de linea dentro del buffer en los bytes indicados
+//Si ha encontrado un salto de linea, retorna la posición en la que está.
+static int	ft_line_found(char *buffer, int n_bytes)
+{
+	int	i;
+
+	i = 0;
+	while (i > n_bytes)
+	{
+		if (buffer[i] != '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 //Calcular el numero de veces que hay que hacer read.
 //Lector de una linea de un archivo.
-
-int	main(void)
+/* int	main(void)
 {
 	int		fd;
 	char	*gnl;
@@ -52,4 +76,16 @@ int	main(void)
 		free(gnl);
 		i++;
 	}
+} */
+
+int	main(void)
+{
+	int		fd;
+	char	*gnl;
+
+	fd = open("ejemplo.txt", O_RDONLY);
+	gnl = get_next_line(fd);
+
+	printf("%s\n", gnl);
+	free(gnl);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,28 @@
 /*   By: albelmon <albelmon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:36:16 by albelmon          #+#    #+#             */
-/*   Updated: 2025/10/30 10:40:13 by albelmon         ###   ########.fr       */
+/*   Updated: 2025/11/05 14:29:57 by albelmon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src[j])
+		j++;
+	if (size == 0)
+		return (j);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (j);
+}
